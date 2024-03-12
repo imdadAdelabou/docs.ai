86873c32f8f1b9da584c79138a1d08a21139eef4
imdad Adelabou <imdadadelabou@imdads-MacBook-Pro.local>
2024-03-12 05:19:09 +0100

    [FEAT] Design the document screen and display a simple text editor using Flutter Quill

--- lib/screens/document/document_screen.dart
+++ lib/screens/document/document_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
 import 'package:google_clone/screens/document/widgets/document_screen_app_bar.dart';
+import 'package:google_clone/utils/colors.dart';
 
 //Snippet : stfl
 class DocumentScreen extends ConsumerStatefulWidget {
@@ -18,6 +21,7 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
   final TextEditingController titleCtrl = TextEditingController(
     text: 'Untitled Document',
   );
+  final QuillController _controller = QuillController.basic();
 
   @override
   Widget build(BuildContext context) {
@@ -25,12 +29,38 @@ class _DocumentScreenState extends ConsumerState<DocumentScreen> {
       appBar: DocumentScreenAppBar(
         titleCtrl: titleCtrl,
       ),
-      body: Column(
-        children: [
-          Text(
-            widget.id,
-          ),
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            const Gap(10.0),
+            QuillToolbar.simple(
+              configurations: QuillSimpleToolbarConfigurations(
+                controller: _controller,
+              ),
+            ),
+            Expanded(
+              child: SizedBox(
+                width: 750,
+                child: Card(
+                  color: kWhiteColor,
+                  surfaceTintColor: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(1.0),
+                  ),
+                  elevation: 5.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: QuillEditor.basic(
+                      configurations: QuillEditorConfigurations(
+                        controller: _controller,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,22 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
 import google_sign_in_ios
+import irondash_engine_context
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
+import super_native_extensions
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  IrondashEngineContextPlugin.register(with: registry.registrar(forPlugin: "IrondashEngineContextPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  SuperNativeExtensionsPlugin.register(with: registry.registrar(forPlugin: "SuperNativeExtensionsPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
