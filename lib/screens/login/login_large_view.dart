4435385c99de8ad5bad723db19f4f2501be33e4c
imdad Adelabou <imdadadelabou@imdads-MacBook-Pro.local>
2024-04-01 05:49:21 +0200

    [FEAT] Add a tracker to the onBoarding slider

--- lib/screens/login/login_large_view.dart
+++ lib/screens/login/login_large_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
 import 'package:google_clone/screens/login/model.dart';
 import 'package:google_clone/utils/app_assets.dart';
 import 'package:google_clone/utils/app_text.dart';
@@ -79,17 +80,77 @@ class _OnBoardingView extends StatelessWidget {
 }
 
 /// The widget to display on large screen for the login screen
-class LoginLargeView extends StatelessWidget {
+class LoginLargeView extends StatefulWidget {
   /// Creates a [LoginLargeView] widget
   const LoginLargeView({super.key});
 
   @override
+  State<LoginLargeView> createState() => _LoginLargeViewState();
+}
+
+class _LoginLargeViewState extends State<LoginLargeView> {
+  int _currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      itemCount: _onBoardingItems.length,
-      itemBuilder: (BuildContext context, int index) => _OnBoardingView(
-        item: _onBoardingItems[index],
-      ),
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: PageView.builder(
+            onPageChanged: (int index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            itemCount: _onBoardingItems.length,
+            itemBuilder: (BuildContext context, int index) => _OnBoardingView(
+              item: _onBoardingItems[index],
+            ),
+          ),
+        ),
+        Center(
+          child: OnBoardingTracker(
+            currentIndex: _currentIndex,
+          ),
+        ),
+        const Gap(30)
+      ],
+    );
+  }
+}
+
+/// The widget to track the slider item between the value of _OnboardingItems
+class OnBoardingTracker extends StatelessWidget {
+  /// Creates a [OnBoardingTracker] widget
+  const OnBoardingTracker({
+    required this.currentIndex,
+    this.radius = 7,
+    super.key,
+  });
+
+  /// The radius of the circle
+  final double radius;
+
+  /// The current index of the slider
+  final int currentIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: _onBoardingItems
+          .map(
+            (elm) => Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: CircleAvatar(
+                radius: radius,
+                backgroundColor: currentIndex == _onBoardingItems.indexOf(elm)
+                    ? kBlueColorVariant
+                    : kWhiteColor,
+              ),
+            ),
+          )
+          .toList(),
     );
   }
 }
